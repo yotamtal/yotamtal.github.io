287f81088a6336259fe80c8c05f5d19059fd9496
Yotam Tal <yotam978@gmail.com>
2018-09-11 23:43:46 +0300

    x

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-// var path = document.querySelector(".st3");
-// var total_length = path.getTotalLength();
-// console.log(total_length); 
+var path = document.querySelector(".st3");
+var total_length = path.getTotalLength();
+console.log(total_length); 
 
 //
 // Flexbox transition animations by Gabi
